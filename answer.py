31b488f9f0c67be0d3d348157f57460e368b0353
Ben Beckom <benbeckom@gmail.com>
2019-01-06 09:22:36 -0700

    Initial commit

---
+++ answer.py
@@ -0,0 +1,44 @@
+def Uanswer(optionstotal):
+    #Output a number for answer to question. Checking to ensure number is chosen.
+    while True:
+        answer = input(": ")
+        try:
+            val = int(answer)
+            if val in range(1,optionstotal+1):
+                print("")
+                return val
+            else:
+                print("not an option")
+                continue
+        except:
+            print("put in a number")
+            continue
+
+class branch1(object):
+    #First question branch
+    def __init__(self,value):
+        self.value = value
+
+    def output(self):
+        if self.value == 1:
+            return "You are shot at once!"
+        if self.value == 2:
+            return "You're shot at twice!"
+        if self.value == 3:
+            return "You're shot at three times!"
+        if self.value == 4:
+            return "You're shot at four times!"
+
+class branch2(object):
+    #Second question branch
+    def __init__(self,value,value2):
+        self.value = value
+        self.value2 = value2
+
+    def output(self):
+        if self.value == 1:
+            return "You die"
+        if self.value == 2 and self.value2 == 1:
+            return "You dodged the bullet!"
+        if self.value == 2 and self.value2 > 1:
+            return "You dodge "+str(self.value2)+" bullets!"
---
+++ main.py
@@ -0,0 +1,37 @@
+from time import sleep
+import sys
+
+import answer
+
+intro = ('----------\n'
+          'Test  Game\n'
+          '----------\n')
+
+for x in intro:
+      print(x, end='')
+      sys.stdout.flush()
+      sleep(0.03)
+print('\n')
+
+#start game
+while True:
+      #question 1
+      print("select:\n"
+            "1. Go left\n"
+            "2. Go right\n"
+            "3. Go forwards\n"
+            "4. Go backwards\n")
+      value1 = answer.Uanswer(4)
+      answer1 = answer.branch1(value1)
+      print(answer1.output())
+
+      #question 2
+      print("Get shot?:\n"
+            "1. Yes\n"
+            "2. No\n")
+      value2 = answer.Uanswer(2)
+      answer2 = answer.branch2(value2,value1)
+      print(answer2.output())
+
+      #finished
+      break
